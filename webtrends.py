327b0b979aa76a1aa819e3c527a0c5e3fddd6124
Chad <chad@Chads-MacBook-Pro.local>
2016-02-01 12:20:09 -0800

    added webtrends Data Extraction endpoints

--- webtrends.py
+++ webtrends.py
@@ -1,1 +1,28 @@
-__author__ = 'chad'
+import requests
+
+def get_report_data():
+    pass
+
+def get_report_meta():
+    pass
+
+def list_spaces():
+    pass
+
+def list_profiles():
+    pass
+
+def list_reports():
+    pass
+
+
+### START Methods TBD (Probably won't need these for a while)
+def get_key_metrics_for_a_profile():
+    pass
+
+def get_key_metrics_for_all_profiles_in_a_space():
+    pass
+
+def get_key_metrics_for_default_profiles_of_all_spaces():
+    pass
+### END Methods TBD
