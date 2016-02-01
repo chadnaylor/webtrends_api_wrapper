02ca67b2881b7ba09f0858eaab674cf2a1cce23c
Chad <chad@Chads-MacBook-Pro.local>
2016-02-01 13:11:40 -0800

    added dummy test methods

--- tests.py
+++ tests.py
@@ -1,7 +1,31 @@
 import unittest
 
 class TestWebtrendsAPIWrapper(unittest.TestCase):
-    pass
+    def test_get_report_data(self):
+        pass
+    
+    def test_get_report_meta(self):
+        pass
+    
+    def test_list_spaces(self):
+        pass
+    
+    def test_list_profiles(self):
+        pass
+    
+    def test_list_reports(self):
+        pass
+
+    ### START Methods TBD (Probably won't need these for a while)
+    def test_get_key_metrics_for_a_profile(self):
+        pass
+    
+    def test_get_key_metrics_for_all_profiles_in_a_space(self):
+        pass
+    
+    def test_get_key_metrics_for_default_profiles_of_all_spaces(self):
+        pass
+    ### END Methods TBD
 
 if __name__ == '__main__':
     unittest.main()
